a56f440f14f950dfcf99c5b0bbc29ed9f3714fb3
rajalakshmi.v <rajalakshmi.v15@iiits.in>
2020-11-02 15:42:02 +0000

    Update init_db.sql

--- init_db.sql
+++ init_db.sql
@@ -1,5 +1,5 @@
-CREATE IF NOT EXISTS DATABASE employee_db;
+CREATE DATABASE IF NOT EXISTS employee_db;
 GRANT ALL ON *.* to db_user@localhost IDENTIFIED BY 'Passw0rd';
 USE employee_db;
-CREATE TABLE employees (name VARCHAR(20));
+CREATE TABLE IF NOT EXISTS employees (name VARCHAR(20));
 INSERT INTO employees VALUES ('JOHN');
